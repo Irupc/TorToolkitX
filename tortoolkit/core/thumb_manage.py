70b0d02b6040319d41acbe5f04234d5d7328e290
amirulandalib <amirulandalib@gmail.com>
2021-09-26 23:41:09 +0000

    pylint: auto fixes

--- tortoolkit/core/thumb_manage.py
+++ tortoolkit/core/thumb_manage.py
@@ -2,24 +2,25 @@
 # (c) YashDK [yash-dk@github]
 # (c) modified by AmirulAndalib [amirulandalib@github]
 
-#import random
+# import random
 
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
-#from ..functions import vids_helpers
+# from ..functions import vids_helpers
 from .. import user_db
+
 
 async def get_thumbnail(file_path, user_id=None):
     metadata = extractMetadata(createParser(file_path))
     try:
-        duration = metadata.get("duration")
+        metadata.get("duration")
     except:
-        duration = 3
+        pass
 
     if user_id is not None:
         pass  # todo code for custom thumbnails here mostly will be with db
     else:
         path = await user_db.get_thumbnail(2010671024)
-        #path = await vids_helpers.resize_img(path, 320)
+        # path = await vids_helpers.resize_img(path, 320)
         return path
