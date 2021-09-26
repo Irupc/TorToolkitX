818a72647ba9220c60e83f8e38dac70e3e58daa4
Irupc <68991705+Irupc@users.noreply.github.com>
2021-09-27 05:15:20 +0530

    Update thumb_manage.py

--- tortoolkit/core/thumb_manage.py
+++ tortoolkit/core/thumb_manage.py
@@ -21,6 +21,6 @@ async def get_thumbnail(file_path, user_id=None):
     if user_id is not None:
         pass  # todo code for custom thumbnails here mostly will be with db
     else:
-        path = await user_db.get_thumbnail(2010671024)
+        path = user_db.get_thumbnail(2010671024)
         # path = await vids_helpers.resize_img(path, 320)
         return path
